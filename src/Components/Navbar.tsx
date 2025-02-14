4c0c09142467193db8f56b51d9a84fcfcf429b79
reshmanr <reshmanr2000@gmail.com>
2025-02-14 10:37:39 +0530

    Updated navbar design and structure

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,33 +3,33 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <header >
-      <nav > 
-        <div >
-          Blog Logo
+
+      <nav className='flex justify-around font-bold py-8 bg-gray-400'> 
+        <div className='font-serif text-3xl '>
+          A Slice of life.
         </div>
         
        
-        <ul>
-          <li>
+        <ul className='flex gap-6 font-mono text-xl '>
+          <li className='hover:border-b-2 border-black '>
             <NavLink 
               to="/" >
               Home
             </NavLink>
           </li>
-          <li>
+          <li className='hover:border-b-2 border-black '>
             <NavLink 
               to="/travel" >
               Travel
             </NavLink>
           </li>
-          <li>
+          <li className='hover:border-b-2 border-black '>
             <NavLink 
               to="/food" >
               Food
             </NavLink>
           </li>
-          <li>
+          <li className='hover:border-b-2 border-black '>
             <NavLink 
               to="/diy" >
               DIY
@@ -37,7 +37,7 @@ const Navbar: React.FC = () => {
           </li>
         </ul>
         
-        <div>
+        <div className="font-mono text-xl hover:border-b-2 border-black ">
             <NavLink 
               to="/profile" >
               Profile
@@ -45,7 +45,7 @@ const Navbar: React.FC = () => {
         </div>
     
       </nav>
-    </header>
+
   );
 };
 
