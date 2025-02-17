083e81489f8d9ee8d200c038d3ce30eff6a15f6a
reshmanr <reshmanr2000@gmail.com>
2025-02-17 09:30:37 +0530

    Add PopularPost component

---
+++ src/Components/Home/Popular.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../../Service/api'
+import { Link } from 'react-router-dom';
+
+interface Post {
+  id: string;
+  title: string;
+  image: string;
+}
+
+const PopularPosts = () => {
+  const { data: popularPosts, isLoading,error  } = useQuery<Post[]>({
+    queryKey: ['PopularPosts'],
+    queryFn: async () => {
+      const res = await api.get('/?popular=true');
+      console.log('API Response:', res.data); 
+      return res.data;
+    },
+  });
+
+  if (isLoading) return <p>Loading Popular Posts...</p>;
+  if (error) return <p>Error loading Popular posts: {error.message}</p>;
+
+  return (
+    <div className='m-11'>
+      <h2 className="text-4xl font-mono font-bold my-8 ">Popular Posts</h2>
+      <div className="grid gap-15">
+        {popularPosts?.map((post: Post) => (
+          <Link to={`/post/${post.id}`} key={post.id}>
+            <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow">
+              <img src={post.image} alt={post.title} className="w-full h-48 object-cover rounded-t-lg" />
+              <div className="p-4">
+                <h3 className="font-semibold text-lg">{post.title}</h3>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularPosts;
